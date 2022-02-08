b82350883fea4f088465f6776652b3a6d9841a36
glatt <48278735+geraldglatt@users.noreply.github.com>
2022-02-08 12:33:21 +0100

    Add files via upload

---
+++ weapon.php
@@ -0,0 +1,20 @@
+<?php
+
+$weapons = ['fists', 'whip', 'gun'];
+$opponentWeapon = $weapons[rand(0,2)]; // Cela permet de choisir une arme de manière aléatoire.
+switch($opponentWeapon){
+    case 'fists' : $stevensonWeapon =  $weapons[2] ;
+    break;
+    case 'whip' : $stevensonWeapon = $weapons[2] ;
+    break;
+    case 'gun' :$stevensonWeapon = $weapons[2] ;
+    break;
+    default : echo "Ce case ne correspond à rien !";
+    break;
+
+}
+ echo "L'arme de stevenson est : ".$stevensonWeapon;
+
+
+
+
