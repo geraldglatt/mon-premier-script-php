b3fe4e80c0453298d0fdd044f03b1b3544e56e12
glatt <48278735+geraldglatt@users.noreply.github.com>
2022-02-10 21:42:01 +0100

    Add files via upload

---
+++ thanks.php
@@ -0,0 +1,4 @@
+
+Merci {$_POST[user_surname]} {$_POST[user_name]} de nous avoir contacté à propos de “{$_POST[language]}”.
+Un de nos conseiller vous contactera soit à l’adresse {$_POST[user_email]} ou par téléphone au {$_POST[user_phone]} dans les plus brefs délais pour traiter votre demande : 
+{$_POST[user_message]}
